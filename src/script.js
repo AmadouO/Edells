02411a404324b2b44181040ffa7d26f29bb00e31
Edells <aourys14@yahoo.com>
2024-02-09 13:59:07 +0100

    Premier envoie sur github

---
+++ src/script.js
@@ -0,0 +1,9 @@
+const h = document.querySelector("h1");
+const image = document.querySelector("img");
+const pargr = document.querySelector("p");
+const sectn = document.querySelector("section");
+const lien = document.querySelector("a");
+const input = document.querySelector("input");
+
+h.childNodes[0].nodeValue = "bonjour Edells";
+console.log(h.childNodes[0]);
