c82d1c0960532c4c6408c9d32579463d058cc0b8
Edells <aourys14@yahoo.com>
2024-02-12 20:28:49 +0100

    premier commit de notre todo liste

--- src/script.js
+++ src/script.js
@@ -1,10 +1,1 @@
-const h = document.querySelector("h1");
-const image = document.querySelector("img");
-const pargr = document.querySelector("p");
-const sectn = document.querySelector("section");
-const lien = document.querySelector("a");
-const input = document.querySelector("input");
-
-h.childNodes[0].nodeValue = "bonjour Edells";
-
-console.log(h.childNodes[0]);
+import "./style.css";
